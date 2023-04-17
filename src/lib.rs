2eaab7ef19f9fa39107289562dc0d94c49295990
Eamon <eamon.r.burns@gmail.com>
2023-04-17 09:40:19 -0700

    - Added `Controls` struct - The `Controls` struct contains all the `KeyCode`s for the controls

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,27 @@
 use std::{io::{stdout, Write}, error::Error};
 use crossterm::event::{self, Event, KeyCode};
 
+pub struct Controls {
+    toggle_menu: KeyCode,
+    toggle_start: KeyCode,
+    reset_time: KeyCode,
+    switch_time: KeyCode,
+    quit: KeyCode,
+    settings: KeyCode,
+}
 
 pub fn run() -> Result<(), Box<dyn Error>> {
     crossterm::terminal::enable_raw_mode().expect("oh no");
     let mut stdout = stdout();
 
-    
+    let default_controls = Controls {
+        toggle_menu: KeyCode::Esc,
+        toggle_start: KeyCode::Char('s'),
+        reset_time: KeyCode::Char('r'),
+        switch_time: KeyCode::Char(' '),
+        quit: KeyCode::Char('q'),
+        settings: KeyCode::Char('s'),
+    };
 
     loop {
         // Wait for a key event
