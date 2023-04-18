5a5d935af1822fa02f4e5fa4f551748259482cf6
Eamon <eamon.r.burns@gmail.com>
2023-04-17 18:13:12 -0700

    - Added control detection - If the pressed key is one of the keys defined in `default_controls`, the control will be printed

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,10 @@ use crossterm::event::{self, Event, KeyCode};
 pub struct Controls {
     toggle_menu: KeyCode,
     toggle_start: KeyCode,
+    toggle_settings: KeyCode,
     reset_time: KeyCode,
     switch_time: KeyCode,
     quit: KeyCode,
-    settings: KeyCode,
 }
 
 pub fn run() -> Result<(), Box<dyn Error>> {
@@ -17,10 +17,10 @@ pub fn run() -> Result<(), Box<dyn Error>> {
     let default_controls = Controls {
         toggle_menu: KeyCode::Esc,
         toggle_start: KeyCode::Char('s'),
+        toggle_settings: KeyCode::Char('s'),
         reset_time: KeyCode::Char('r'),
         switch_time: KeyCode::Char(' '),
         quit: KeyCode::Char('q'),
-        settings: KeyCode::Char('s'),
     };
 
     loop {
@@ -32,8 +32,13 @@ pub fn run() -> Result<(), Box<dyn Error>> {
                 if key.kind != crossterm::event::KeyEventKind::Press { continue; }
 
                 match key.code {
-                    KeyCode::Char('q') => break,
-                    k => print!("{k:?}"),
+                    qu if qu == default_controls.quit => break,
+                    tm if tm == default_controls.toggle_menu => print!("Toggle menu\n\r"),
+                    ts if ts == default_controls.toggle_start => print!("Toggle start\n\r"),
+                    rt if rt == default_controls.reset_time => print!("Reset time\n\r"),
+                    st if st == default_controls.switch_time => print!("Switch time\n\r"),
+                    ts if ts == default_controls.toggle_settings => print!("Toggle settings\n\r"),
+                    k => print!("{k:?}\n\r"),
                 }
             }
             _ => (),
